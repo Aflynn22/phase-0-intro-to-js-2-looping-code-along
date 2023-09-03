85b5584da7da5083913caa74c7762f1bbc0fecde
Austin <Austin.flynn1992@gmail.com>
2023-09-02 19:33:46 -0500

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = (["Guadalupe", "Ollie", "Aki"], "surprise");
+function writeCards(names){
+    const names2 = []
+    let i = 0; 
+    while (i < names.length){
+        console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        names2.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        i++
+    }
+    return names2
+}
+
+function countDown(number){
+    if(number >= 0){
+        console.log(number);
+        number--;
+        countDown(number)
+    }
+}
